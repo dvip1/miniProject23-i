e0ff02ebd1cb93bff34d65e0b1a66bc31c0bfc30
dvip1 <dvip.d.patel@slrtce.in>
2023-09-02 22:27:12 +0530

    connecting frontend+backend

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,10 +1,24 @@
 import '../components/homeStyles.css';
-import RootNav from '../components/partials/homeNav';
+import HomeNav from '../components/partials/homeNav';
 export default function Home() {
+    const fetchData = async (buttonValue:any) => {
+        const response = await fetch('http://localhost:5000/get_data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                button_value: buttonValue
+            })
+        });
+        const data = await response.json();
+        console.log(data);
+    }
     return (
         <>
             <div className="my-container">
-                <RootNav />
+                
+                <HomeNav />
                 <div className="center">
                     <h1>Welcome to TradeVista</h1>
                     <p className='description'>Your gateway to Financial Proficiency</p>
@@ -12,8 +26,8 @@ export default function Home() {
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi unde iure, vitae illum eligendi excepturi quaerat similique. Sapiente dolorum, earum eveniet exercitationem facilis ipsam voluptate aliquid corporis autem sint?</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime reprehenderit fugiat ad voluptatem, odio </p>
                     </div>
-                    <button>
-                        Get Started
+                    <button onClick={() => fetchData('AAPL')}>
+                        Test Button
                     </button>
                 </div>
             </div>
--- server/app.py
+++ server/app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import yfinance as yf
+import pandas as pd
+from flask_cors import CORS
+
 app = Flask(__name__)
-from api import hello_world
-app.add_url_rule("/", "hello_world", hello_world)
+CORS(app)  # Use CORS as middleware for your Flask app
+
+@app.route('/get_data', methods=['POST'])
+def get_data():
+    sys = request.form['button_value']
+    print(sys)
+
+    try:
+        data = yf.download(f'{sys}.NS')
+        data.to_csv(f"Data/{sys}.csv")
+        data_json = data.reset_index().to_json(orient='records')
+        return jsonify({'data': data_json})
+    except Exception as e:
+        print(f"Sorry, there has been an error {e} for file {sys}")
+        return jsonify({'error': str(e)})
+
 if __name__ == "__main__":
     app.run(debug=True)
-
