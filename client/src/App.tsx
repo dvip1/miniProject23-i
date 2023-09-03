d9dcee427932a56c23a7f23fe54b81c84d5cbf64
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-09-03 19:26:18 +0530

    Started with the signup page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-import Home from "./routes/Home"
+import Home from "./routes/Home";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import SignUp from "./components/signin/Signup";
 function App() {
   return (
     <>
-    <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
