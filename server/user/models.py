0282b1ce1d9ee3a62855cde586a6d9650ff9f05a
dvip1 <dvip.d.patel@slrtce.in>
2023-10-24 01:54:03 +0530

    user directory

---
+++ server/user/models.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request, session
+import uuid
+from passlib.hash import pbkdf2_sha256
+from user.database import db
+
+class User:
+    def signup(self):
+        try:
+            data = request.get_json()
+            email = data.get('email')
+            password = data.get('password')
+
+            if not email or not password:
+                return jsonify({'error': 'Missing email or password'}), 400
+
+            if db.users.find_one({'email': email}):
+                return jsonify({'error': 'Email address already in use'}), 400
+
+            user = {
+                '_id': uuid.uuid4().hex,
+                'email': email,
+                'password': pbkdf2_sha256.hash(password),
+            }
+
+            if db.users.insert_one(user):
+                session['user_id'] = user['_id']  # Store user ID in session
+                user.pop('password', None)  # Remove password before returning user
+                return jsonify(user), 200
+
+            return jsonify({'error': 'Signup failed'}), 400
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
+    def sign_in(self):
+        try:
+            data = request.get_json()
+            email = data.get('email')
+            password = data.get('password')
+
+            if not email or not password:
+                return jsonify({'error': 'Missing email or password'}), 400
+
+            user = db.users.find_one({'email': email})
+
+            if user and pbkdf2_sha256.verify(password, user['password']):
+                session['user_id'] = user['_id']  # Store user ID in session
+                user.pop('password', None)  # Remove password before returning user
+                return jsonify(user), 200
+
+            return jsonify({'error': 'Invalid login credentials'}), 401
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+    def is_authenticated(self):
+        try:
+            if 'user_id' in session:
+                user = db.users.find_one({'_id': session['user_id']})
+                user.pop('password', None)  # Remove password before returning user
+                return jsonify(user), 200
+            else:
+                return jsonify({'error': 'No user is currently signed in'}), 400
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
---
+++ server/user/sign_up.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request, jsonify
+sign_up_api = Blueprint('sign_up_api', __name__)
+from user.models import User
+@sign_up_api.route('/sign_up', methods=['POST'])
+def sign_up():
+    user = User()
+    return user.signup()
+
+@sign_up_api.route('/sign_up', methods=['OPTIONS'])
+def sign_up_preflight():
+    return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
+
