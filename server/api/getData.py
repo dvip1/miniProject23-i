36dccd5ea7de550e6e46dfd86372dd9f25d6a508
dvip1 <dvip.d.patel@slrtce.in>
2023-10-23 11:04:31 +0530

    minor changes

--- server/api/getData.py
+++ server/api/getData.py
@@ -4,16 +4,17 @@ import pandas as pd
 
 getData_api = Blueprint('getData_api', __name__)
 
-@getData_api.route('/getData', methods=['POST'])
+@getData_api.route('/get_data', methods=['POST'])
 def get_data():
     sys = request.form['button_value']
     print(sys)
 
     try:
         data = yf.download(f'{sys}.NS')
-        data.to_csv(f"../Data/{sys}.csv")
+        data.to_csv(f"Data/{sys}.csv")
         data_json = data.reset_index().to_json(orient='records')
         return jsonify({'data': data_json})
     except Exception as e:
         print(f"Sorry, there has been an error {e} for file {sys}")
         return jsonify({'error': str(e)})
+
