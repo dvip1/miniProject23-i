2a1b566bf3294adb629d53411396e09a92f3e68a
Dvip <98635463+dvip1@users.noreply.github.com>
2023-11-02 22:38:49 +0530

    visual changes iii

--- client/src/components/partials/homeNav.tsx
+++ client/src/components/partials/homeNav.tsx
@@ -56,8 +56,8 @@ export default function RootNav() {
 
   return (
     <>
-
-      <nav className="bg-blue-500">
+      <div className="bg-blue-500 flex justify-center ">
+      <nav className="max-w-3xl container" >
         <div className="flex justify-start">
           <NavLink to="/" className="navBrand">
             <span className="bold">trade</span>vista
@@ -66,31 +66,10 @@ export default function RootNav() {
         <div className="flex justify-center items-center">
           <NavLink
             to="/profile"
-            className="shadow-sm   flex justify-center items-center ml-2 p-1 md:p-3 bg-[#89CFF3] rounded-full mx-1"
+            className="shadow-sm  flex justify-center items-center ml-2 p-3 md:p-5 bg-[#D0E7D2] rounded-full mx-1 text-2xl"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-10 h-10"
-
-            />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-10 h-10"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
+            <i className="fa-solid fa-user"></i>  
+           
             </NavLink>
             <NavLink
               to="/dashboard"
@@ -108,6 +87,7 @@ export default function RootNav() {
               : null}
           </div>
         </nav>
+        </div>
     </>
   );
 }
